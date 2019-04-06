9e5b7f1b3bed57252da9eba534c716baa071be0c
Skyler Rankin <skylerrankin@gmail.com>
2019-04-06 14:47:46 -0400

    component data

---
+++ script.js
@@ -0,0 +1,62 @@
+var components = [
+    {
+        "name": "AnimationComponent",
+        "params": ["delay", "width", "height", "files", "lengths", "flips", "loop"]
+    },
+    {
+        "name": "CameraFollowComponent",
+        "params": ["mode"]
+    },
+    {
+        "name": "DirectionalHistboxComponent",
+        "params": ["width", "height", "x", "y", "offset"]
+    },
+    {
+        "name": "DoorComponent",
+        "params": []
+    },
+    {
+        "name": "EnemeyComponent",
+        "params": ["speed", "damage"]
+    },
+    {
+        "name": "HealthComponent",
+        "params": ["max health", "health"]
+    },
+    {
+        "name": "InteractableComponent",
+        "params": []
+    },
+    {
+        "name": "InteractionTrigger",
+        "params": []
+    },
+    {
+        "name": "MirrorComponent",
+        "params": ["direction"]
+    },
+    {
+        "name": "MovementComponent",
+        "params": ["x", "y", "velocity x", "velocity y", "width", "height", "walk force", "dash force", "data", "is boundary", "hits directional hitbox", "id"]
+    },
+    {
+        "name": "ParallaxLayer",
+        "params": ["x", "y", "image"]
+    },
+    {
+        "name": "PlayerInputComponent",
+        "params": []
+    },
+    {
+        "name": "PlayerStateComponent",
+        "params": []
+    },
+    {
+        "name": "RawPositionComponent",
+        "params": []
+    },
+    {
+        "name": "RenderComponent",
+        "params": ["width", "height"]
+    },
+]
